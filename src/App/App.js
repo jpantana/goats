b772f9da2fd944bff09c3c721cb4981ec15c19a7
JPantana <joshpantana@me.com>
2019-06-17 19:22:45 -0500

    importing goats data

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import goats from './goats';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className="btn btn-danger">GOATS</button>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    goats: [],
+  }
+
+  componentDidMount() {
+    this.setState({goats})
+  }
+
+  render() {
+    // javascripty things here
+    return (
+      <div className="App">
+        <div className="">Goat yoga</div>
+        {/* <GoatCorral goats={goats}/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/App/goats.js
@@ -0,0 +1,54 @@
+const goats = [
+  {
+    id: 'goat1',
+    name: 'Billy',
+    disposition: 'rude',
+    imageUrl: 'https://cdn.pixabay.com/photo/2012/06/19/15/06/goat-50290_1280.jpg',
+    fullness: 67,
+    favPose: 'downward facing dog',
+    isFainting: true,
+    color: 'black',
+  },
+  {
+    id: 'goat2',
+    name: 'Ralph',
+    disposition: 'passive',
+    imageUrl: 'https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1554932989/shape/mentalfloss/istock-482891144-goat-eyes.jpg',
+    fullness: 32,
+    favPose: 'molting spider',
+    isFainting: false,
+    color: 'blue',
+  },
+  {
+    id: 'goat3',
+    name: 'Monica',
+    disposition: 'sassy',
+    imageUrl: 'https://zoosauvage.org/app/uploads/2017/11/chevre-domestique-1.jpg',
+    fullness: 87,
+    favPose: 'twisted villain',
+    isFainting: true,
+    color: 'brown',
+  },
+  {
+    id: 'goat4',
+    name: 'Damion',
+    disposition: 'eccentric',
+    imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUoMaI0gCIRoYcUSh4BfVKcBF813uN7PYSVOc3h5F88PwhAlua',
+    fullness: 23,
+    favPose: 'upside-down tomato',
+    isFainting: true,
+    color: 'brown',
+  },
+  {
+    id: 'goat5',
+    name: 'Ricky',
+    disposition: 'light-hearted',
+    imageUrl: 'https://cdn.pixabay.com/photo/2018/05/19/03/26/goat-3412678__340.jpg',
+    fullness: 50,
+    favPose: 'the alabaster twist',
+    isFainting: false,
+    color: 'tan',
+  }
+  ];
+
+  export default goats;
